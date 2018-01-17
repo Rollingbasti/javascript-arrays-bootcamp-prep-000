743568f3eb3cdc489cd0a2d2bfaacfb8b9f675ed
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-17 07:58:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0,array.length-1)
+  var test = array.slice(0,array.length-1)
   return array;
 }
